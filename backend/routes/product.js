273f7605718b5fe36a5d27906dacfb7816f027b8
bibash <163483962+bibash35@users.noreply.github.com>
2025-07-11 16:31:02 +0545

    Update product.js

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,25 +1,3 @@
-// const {
-//     storeProduct,
-//     updateProduct,
-//     deleteProduct,
-//     fetchProducts,
-//     }=require("../controller/product")
-// const express = require("express");
-// const { isSeller, checkAuthentication } = require("../middelware/ticket");
-
-
-
-  
-
-// const router = express.Router();
-// router.get("",fetchProducts)
-// router.post("",checkAuthentication,isSeller,storeProduct)
-// router.put("/:_id",checkAuthentication,isSeller,updateProduct)
-// router.delete("/:_id",checkAuthentication,isSeller,deleteProduct)
-
-
-// module.exports = router;
-
 const express = require("express");
 const {
   storeProduct,
